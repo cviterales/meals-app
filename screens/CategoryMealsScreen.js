8154e7514fbe476fa1cc5ddcda7ad197821abfea
Cristian Viterales <cviterales@batan.coop>
2020-11-17 00:11:02 -0300

    added screens and react navigation

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meals Screen!</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoryMealsScreen;
