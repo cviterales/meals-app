0480eba01d55ae2265e33cbdc7e1c32fde5f6f6b
Cristian Viterales <cviterales@batan.coop>
2020-11-17 21:21:02 -0300

    added flatList on CategoriesScreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import DeprecatedViewPropTypes from "react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go to Meals"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-    </View>
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+      keyExtractor={(item) => item.id}
+    />
   );
 };
 
@@ -22,6 +28,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+
+  }
 });
 
 export default CategoriesScreen;
